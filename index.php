5530152e082e1c20d7a95dde1ce22f55781b797b
DanaoJaredSimon <jaredsimondanao@gmail.com>
2022-04-17 11:30:37 +0800

    Index has been modified

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-2">
+                <img src="image/logo.png" width="40" height="40">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+               </ul>
+                <a class="navbar-brand" href="#page-top"> Musify </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song_5</a></li>
+                    </ul>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+     
+        <header class=" bg-dark bg-gradient text-white">
+            <div class="container py-0 px-3 text-center">
+                <img src="image/logo.png" width="90" height="90" </img>
+                <h1 class="fw-bolder">Welcome to Musify</h1>
+                <p class="lead">This songs is all about love</p>
+                
+            </div>
+                        <style>
+        body {
+                 background: #654ea3;
+    background: linear-gradient(to right, #e96443, #904e95);
+            }
+        </style>
+
+     
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-2 ">
+                <div class="row gx-2 justify-content-center">
+                    <div class="col-lg-12 text-center text-white">
+                       
+                        <h2 class='fw-bolder'>"SONG LISTS"</h2><br>
+                        <br/>
+                        <div class="py-7 px-9">
+                        <ul class="list-inline-item mb-0" type='none'>
+                        <li><h7 class='fw-bolder'>Song 1 - "Imagination"</h7><i> - Shawn Mendes</i></li><br>
+                        <li><h7 class='fw-bolder'>Song 2- "Kagome" </h7><i> - Loki</i></li><br>
+                        <li><h7 class='fw-bolder'>Song 3-"Kung 'Di Na Ako"</h7><i> - Agsunta</i></li><br>
+                        <li><h7 class='fw-bolder'>Song 4- "Huling Sandali"</h7><i> - December Avenue</i></li><br>
+                        <li><h7 class='fw-bolder'>Song 5- "Sleep Tonight" </h7><i> - December Avenue</i></li><br>
+                        
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-info">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jared Simon R. Danao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
