a46d563aab8b11b6937904a68ad0efec28fb8d26
DanaoJaredSimon <jaredsimondanao@gmail.com>
2022-04-17 11:42:50 +0800

    Song 2 has been added

---
+++ song2.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-2">
+                <img src="image/logo.png" width="40" height="40">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+               </ul>
+                <a class="navbar-brand" href="#page-top">Musify</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+
+        <style>
+        body {
+                 background: #654ea3;
+    background: linear-gradient(to right, #e96443, #904e95);
+            }
+        </style>
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-0 mb-0 text-center">
+
+                <img src="image/logo.png" width="70" height="70">
+                <h3 class="lead">SONG 2</h3>
+
+                <?php echo "<br/>" ?>
+                 <h5 class="lead">TITTLE</h5>
+                <h1 class='font-italic'>"KAGOME"</h1> 
+                <i>"Loki" Lyrics</i></li><br>
+
+                
+            </div>
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-2 justify-content-center">
+                    <div class="col-lg-12 text-center text-white">
+
+                        <?php
+                        echo "<h2 class='text-white'>'LYRICS'</h2>";  
+                        echo "<p class='lead'></p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Eh paano kung hindi</p>";
+                        echo "<p>Siguro nakakulong pa din sa nakaraan</p>";
+                        echo "<p>Hindi ka nakilala</p>";
+                        echo "<p>'Di makalaya</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Pre Chorus]</p>";
+                        echo "<p>Ang sarili dinadaya yeah</p>";
+                        echo "<p>Naglalasing-lasing hindi pala kaya yeah</p>";
+                        echo "<p>Ayokong magising nang 'di ka kasama</p>";
+                        echo "<p>Kung nandito ka sa tabi mas masaya sana</p>";
+                        echo "<p>Ngayon hinahanap ka</p>";
+                        echo "<p>Nasasaktan</p>";
+                        echo "<p>Dinadaan ko lang sa amat</p>";
+                        echo "<p>Ang nararamdaman</p>";
+                        echo "<p>Para naman kahit papa'no gumaan</p>";
+                        echo "<p>Dahil sa totoo lang</p>";
+                        echo "<p>Sa'yo ko lang 'to naramdaman</p>";
+                        echo "<p>Ganito pala ang magmahal</p>";
+                        echo "<p>Mayro'ng kabaliktaran</p>";
+                        echo "<p>Para maintindihan minsan kailangang masaktan hey</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Eh paano kung hindi</p>";
+                        echo "<p>Siguro nakakulong pa din sa nakaraan</p>";
+                        echo "<p>Hindi ka nakilala</p>";
+                        echo "<p>'Di makalaya</p>";
+                        echo "<p>Siguro nakakulong pa din sa nakaraan 'di makalaya</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 1]</p>";
+                        echo "<p>Baby let's go</p>";
+                        echo "<p>One more time</p>";
+                        echo "<p>I can't get you out of my mind yeah</p>";
+                        echo "<p>'Di na makapag hintay yeah</p>";
+                        echo "<p>I just want to get high</p>";
+                        echo "<p>I just wanna see your smile</p>";
+                        echo "<p>I just wanna feel your vibe</p>";
+                        echo "<p>I know it's hard sometimes someday</p>";
+                        echo "<p>But we gonna be alright</p>";
+                        echo "<br/>";
+
+
+                        echo "<p class='small'>[Verse 2]</p>";
+                        echo "<p>Dahil 'pag ikaw ang kasama ay wala na</p>";
+                        echo "<p>Wala nang hahanapin pang iba gusto ko sana</p>";
+                        echo "<p>Gusto ka sana lumalalim ang ating pagsasama</p>";
+                        echo "<p>Kaya dito ka na lang manatili hanggang umaga</p>";
+                        echo "<p>Sino nga siya</p>";
+                        echo "<p>Siya si Kagome na may hawak na mahiwagang hiyas</p>";
+                        echo "<p>Habang ako si Inuyasha</p>";
+                        echo "<p>Nagbago ang tingin ko sa lahat</p>";
+                        echo "<p>Magmula nung masandal ako sa puno ng akasya</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Eh paano kung hindi</p>";
+                        echo "<p>Siguro nakakulong pa din sa nakaraan</p>";
+                        echo "<p>Hindi ka nakilala</p>";
+                        echo "<p>'Di makalaya</p>";
+                        echo "<p>Siguro nakakulong pa din sa nakaraan 'di makalaya</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Pre Chorus]</p>";
+                        echo "<p>Ang sarili dinadaya yeah</p>";
+                        echo "<p>Naglalasing-lasing hindi pala kaya yeah</p>";
+                        echo "<p>Ayokong magising nang 'di ka kasama</p>";
+                        echo "<p>Kung nandito ka sa tabi mas masaya sana</p>";
+                        echo "<br/>";
+
+                         ?>
+
+                        <br>
+                        <p class='text-white fw-bolder'>PLAY</p>
+                        <audio controls>
+                    <source src="Music/Lo Ki - Kagome (Official Music Video).mp3" type="audio/mpeg">
+                </audio> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-info">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jared Simon R. Danao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
