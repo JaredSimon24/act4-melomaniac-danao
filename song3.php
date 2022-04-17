483f967825e0a0b35bc863c83eb9d7b47366dc00
DanaoJaredSimon <jaredsimondanao@gmail.com>
2022-04-17 11:46:46 +0800

    Song has been added

---
+++ song3.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-2">
+                <img src="image/logo.png" width="40" height="40">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+               </ul>
+                <a class="navbar-brand" href="#page-top">Musify</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+
+        <style>
+        body {
+                 background: #654ea3;
+    background: linear-gradient(to right, #e96443, #904e95);
+            }
+        </style>
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-0 mb-0 text-center">
+
+                <img src="image/logo.png" width="70" height="70">
+                <h3 class="lead">SONG 3</h3>
+
+                <?php echo "<br/>" ?>
+                 <h5 class="lead">TITTLE</h5>
+                <h1 class='font-italic'>"KUNG 'DI NA AKO"</h1> 
+                <i>"Agsunta" Lyrics</i></li><br>
+
+                
+            </div>
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-2 justify-content-center">
+                    <div class="col-lg-12 text-center text-white">
+                      
+                      <?php
+                        echo "<h2 class='text-white'>'LYRICS'</h2>";  
+                        echo "<h4 class='text-white'>Guitar Chords</h4>";  
+                        echo "<p class='lead'></p>";
+                        echo "<br/>";
+
+
+                        echo "<p class='small'>[Intro]</p>";
+                        echo "<p>[Dsus2] [D/F#] [Bm] [Gsus2]</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 1]</p>";
+                        echo "<p>[G6] Kitang kita sa iyong mata [Dsus2]</p>";
+                        echo "<p>Na 'di na ako mahalaga [Dsus2]</p>";
+                        echo "<p>Ramdam ko sa iyong mga hawak [Bm]</p>";
+                        echo "<p>Na meron [D/F#] kang binabalak</p>";
+                        echo "<p>[Bm] Haplos na kay lamig</p>";
+                        echo "<p>[D/F#] Tikom na mga bibig</p>";
+                        echo "<p>Wala [Gsus2] na ba talagang [Asus] pag-ibig? [A]</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Kung 'di na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako ang [Asus]</p>";
+                        echo "<p>Magpapatibok ng puso mo [Bm]</p>";
+                        echo "<p>[Gsus2] Paalam na, [Asus] paalam na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako [Asus] ang dahilan</p>";
+                        echo "<p>[Bm] Ayoko na tayong mahirapan pa [Gsus2]</p>";
+                        echo "<p>Paalam [Asus] na</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Interclude:]</p>";
+                        echo "<p>[Gsus2 Gmaj7sus2]</p>";
+                        echo "<br/>";
+                        
+                        echo "<p class='small'>[Verse 2]</p>";
+                        echo "<p>[G6] Wala ng init ang iyong yakap Mauudlot na ang mga pangarap [Dsus2] </p>";
+                        echo "<p>[Gsus2] Mga halik na kay tamis</p>";
+                        echo "<p>Hindi [Bm] man lang dumadaplis [D/F#]</p>";
+                        echo "<p>[Bm] Sana 'di na magtagal</p>";
+                        echo "<p>[D/F#] Kayanin ng dasal</p>";
+                    echo "<p> [Gsus2] Nasaan na ba ang [Asus] pagmamahal? [A]</p>";
+                        echo "<p>Wala [Gsus2] na ba talagang [Asus] pag-ibig? [A]</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Kung 'di na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako ang [Asus]</p>";
+                        echo "<p>Magpapatibok ng puso mo [Bm]</p>";
+                        echo "<p>[Gsus2] Paalam na, [Asus] paalam na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako [Asus] ang dahilan</p>";
+                        echo "<p>[Bm] Ayoko na tayong mahirapan pa [Gsus2]</p>";
+                        echo "<p>Paalam [Asus] na</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Bridge]</p>";
+                        echo "<p>[Bm] Di na kita pililitin pa [D/F#]</p>";
+                        echo "<p>Kung talagang ayaw mo na [Gsus2]</p>";
+                        echo "<p>Basta tandaan mo [Asus]</p>";
+                        echo "<p>Mahal kita [Bm]</p>";
+                        echo "<p>Hindi ka na kailangan mahirapan pa [D/F#]</p>";
+                        echo "<p>Ako na lang [Gsus2]</p>";
+                        echo "<p>Malaya ka na [Asus]</p>";
+                        echo "<p>[Bm] Ohhhhh [F#D] ohhhh Ohhhh [Gsus2] [Asus] Ohhhhh [Asus/Bb]</p>";
+                        echo "<p>[Bm]Kaya pa ba? </p>";
+                        echo "<p>Ipaglalaban [D/F#] pa ba?</p>";
+                        echo "<p> [Gsus2] Di pa nga nag-uumpisa</p>";
+                        echo "<p>Sumuko [Asus] ka na</p>";
+                        echo "<p> [Bm] Nasayang ang lahat</p>";
+                        echo "<p> [D/F#] Tinapon mo nalang</p>";
+                        echo "<p> [Gsus2] Pinagsamahan nating dalawa</p>";
+                        echo "<p> [Asus] Binalewala Ohhhh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>Kung 'di na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako ang [Asus]</p>";
+                        echo "<p>Magpapatibok ng puso mo [Bm]</p>";
+                        echo "<p>[Gsus2] Paalam na, [Asus] paalam na ako [Dsus2]</p>";
+                        echo "<p>Kung 'di na ako [Asus] ang dahilan</p>";
+                        echo "<p>[Bm] Ayoko na tayong mahirapan pa [Gsus2]</p>";
+                        echo "<p>Paalam [Asus] na</p>";
+                        echo "<br/>";
+                        ?>
+
+                        <br>
+                        <p class='text-white fw-bolder'>PLAY</p>
+                        <audio controls>
+                    <source src="Music/Kung 'Di Na Ako - Agsunta - Lyrics.mp3" type="audio/mpeg">
+                </audio> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-info">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jared Simon R. Danao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
